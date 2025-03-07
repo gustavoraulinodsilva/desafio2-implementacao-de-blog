9cfee330c961d3d0d7b479c31664f95214d5ab83
Gustavo Raulino da Silva <gustavoraulinodasilva@gmail.com>
2025-03-07 12:28:14 -0300

    Começando a fazer o menu mobile do site

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,43 @@
-console.log('oi');
+function init() {
+    openMenu();
+    closeMenu();
+    closeOnOverlayClick();
+}
+
+function openMenu() {
+    const menuHamburger = document.querySelector('.ts-menu-hamburger') as HTMLElement;
+    const nav = document.querySelector('.nav') as HTMLElement;
+    const overlay = document.querySelector('.nav-overlay') as HTMLElement;
+
+    menuHamburger.addEventListener('click', () => {
+        nav.classList.add('active');
+        overlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+}
+
+function closeMenu() {
+    const closeButton = document.querySelector('.ts-close-hamburger') as HTMLElement;
+    const nav = document.querySelector('.nav') as HTMLElement;
+    const overlay = document.querySelector('.nav-overlay') as HTMLElement;
+
+    closeButton.addEventListener('click', () => {
+        nav.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+}
+
+function closeOnOverlayClick() {
+    const overlay = document.querySelector('.nav-overlay') as HTMLElement;
+    const nav = document.querySelector('.nav') as HTMLElement;
+
+    overlay.addEventListener('click', () => {
+        nav.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
